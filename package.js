8a16bbfce35ffb9c0e5e94c2f66f4309cea9bfe9
victor-fdez <victor.j.fdez@gmail.com>
2016-04-10 15:05:48 -0600

    update to version 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'victor755:meteor-angular-jade',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Meteor plugin to compile Jade into angular-meteor templates using ecmascript',
   // URL to the Git repository containing the source code for this package.

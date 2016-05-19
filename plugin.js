f2d23912ef8d6be02777ce9281eb833bd10713f4
Victor <victor.j.fdez@gmail.com>
2016-05-18 18:25:54 -0600

    commented code but still having issues with meteor 1.3.2.4

--- plugin.js
+++ plugin.js
@@ -14,37 +14,55 @@ Plugin.registerSourceHandler(
   function(compileStep) 
   {
     var contents = compileStep.read().toString('utf8');
+    /**
+     * setup babel options
+     */
     var babelOptions = Babel.getDefaultOptions();
     babelOptions.sourceMap = false;
     babelOptions.ast = false;
     jadeOpts.filename = compileStep.fullInputPath;
     contents = jade.compile(contents, jadeOpts)();
+    /**
+     * setup paths
+     */
     var newPath = compileStep.inputPath;
     newPath = newPath.replace(/\\/g, "/");
     var newerPath = newPath.replace(".ng.jade", ".js");
     newPath = newPath.replace(".ng.jade", ".html");
-
-    var results = 'import angular from \'angular\'; ' +
-      'import \'angular-meteor\';'+
-      'angular.module(\'angular-meteor\').run([\'$templateCache\', function($templateCache) {' +
-      '$templateCache.put(\'' + newPath + '\', \'' +
-        minify(contents.replace(/'/g, "\\'"), {
-          collapseWhitespace : true,
-          conservativeCollapse : true,
-          removeComments : true,
-          minifyJS : true,
-          minifyCSS: true,
-          processScripts : ['text/ng-template']
-        }) + '\');' +
-    '}]);';
-
+    /**
+     * minify html
+     */
+    var minifiedHtml = minify(
+      contents.replace(/'/g, "\\'"), 
+      {
+        collapseWhitespace : true,
+        conservativeCollapse : true,
+        removeComments : true,
+        minifyJS : true,
+        minifyCSS: true,
+        processScripts : ['text/ng-template']
+      }
+    );
+    /**
+     * generate javascript 
+     */
+    var results = `
+      import angular from 'angular';
+      import 'angular-templates';
+      angular.module('angular-templates').run(['$templateCache', function($templateCache) {
+        $templateCache.put('${newPath}', '${minifiedHtml}');
+      }]);`;
+    /**
+     * generate es6 javascript
+     */
+    console.log('compiling : '+compileStep.inputPath+' -> '+newPath);
+    console.log(`input file: ${results}`);
     var compileResult = Babel.compile(
       results,
       babelOptions
     );
-
     compileStep.addJavaScript({
-      path : newerPath,
+      path : newPath,
       data : compileResult.code, //results.replace(/\n/g, '\\n'),
       sourcePath : compileStep.inputPath
     });
